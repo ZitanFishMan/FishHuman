7f079ab9e265a7e29c8a17daea4461bcf0a0c487
紫檀 <44179827@qq.com>
2017-08-13 23:03:05 +0800

    创建xcode项目

---
+++ FishHuman/FishHuman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FishHuman
+//
+//  Created by 紫檀 on 2017/8/13.
+//  Copyright © 2017年 紫檀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FishHuman/FishHuman/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FishHuman
+//
+//  Created by 紫檀 on 2017/8/13.
+//  Copyright © 2017年 紫檀. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
